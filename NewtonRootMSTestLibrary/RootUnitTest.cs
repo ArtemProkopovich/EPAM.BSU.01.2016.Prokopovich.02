e97b80e81d4cc61524f21646566821a3bd182fd1
ArtemProkopovich <artemprokopovich96@gmail.com>
2016-03-13 13:38:48 +0300

    Добавлены MSUnit и NUnit тесты для функции Newton.Root

--- NewtonRootMSTestLibrary/RootUnitTest.cs
+++ NewtonRootMSTestLibrary/RootUnitTest.cs
@@ -8,8 +8,56 @@ namespace NewtonRootMSTestLibrary
     public class RootUnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Root_WithNumber_4_Power_2_Return_2()
         {
+            double result = Newton.Root(4.0, 2);
+            Assert.IsTrue(Math.Abs(result - Math.Pow(4.0, 1.0/2)) < 0.000001);
+        }
+
+        [TestMethod]
+        public void Root_WithNumber_Neg9_Power_3_Return_Neg3()
+        {
+            double result = Newton.Root(-9.0, 3);
+            Assert.IsTrue(Math.Abs(result - -Math.Pow(9.0, 1.0/3)) < 0.000001);
+        }
+
+        [TestMethod]
+        public void Root_WithNumber_0_Power_3_Return_0()
+        {
+            double result = Newton.Root(0, 3);
+            Assert.IsTrue(Math.Abs(result - Math.Pow(0, 1.0 / 3)) < 0.000001);
+        }
+
+        [TestMethod]
+        public void Root_WithNumber_10_Power_1_Return_10()
+        {
+            double result = Newton.Root(10, 1);
+            Assert.IsTrue(Math.Abs(result - Math.Pow(10, 1.0)) < 0.000001);
+        }
+
+        [TestMethod]
+        public void Root_WithNumberLessAccuracy_Power_10_ReturnEqualsMath()
+        {
+            double result = Newton.Root(0.0000001, 10);
+            Assert.IsTrue(Math.Abs(result - Math.Pow(0.0000001, 1.0/10)) < 0.000001);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void Root_WithPower_0_ReturnException()
+        {
+            Newton.Root(5, 0);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void Root_WithNegNumber_Power_2_ReturnException()
+        {
+            Newton.Root(-5, 2);
+        }
+
+        [TestMethod, ExpectedException(typeof(OverflowException))]
+        public void Root_WithNumber_100_Power_10000_ReturnException()
+        {
+            Newton.Root(100, 10000);
         }
     }
 }
--- NewtonRootNUnitTestLibrary/RootUnitTest.cs
+++ NewtonRootNUnitTestLibrary/RootUnitTest.cs
@@ -10,6 +10,42 @@ namespace NewtonRootNUnitTestLibrary
 {
     [TestFixture]
     public class RootUnitTest
-    { 
+    {
+        [Test]
+        [TestCase(4.0, 2)]
+        [TestCase(0, 3)]
+        [TestCase(10, 1)]
+        [TestCase(0.00000001, 10)]
+        public void Root_CorrectEvulationTest(double num, int pow)
+        {
+            Assert.IsTrue(Math.Abs(Newton.Root(num, pow) - Math.Pow(num, 1.0/pow)) < 0.000001);
+        }
+
+        [Test]
+        [TestCase(-9, 3)]
+        public void Root_NegNumber_OddPower_Test(double num, int pow)
+        {
+            Assert.IsTrue(Math.Abs(Newton.Root(num, pow) - -Math.Pow(-num, 1.0 / pow)) < 0.000001);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentException))]
+        [TestCase(5, 0)]
+        [TestCase(-5, 2)]
+        public void Root_ArgumentExceptionTest(double num, int pow)
+        {
+            Newton.Root(num, pow);
+        }
+
+        [Test, ExpectedException(typeof(ArgumentException))]
+        public void Root_ArgumentExceptionTest()
+        {
+            Newton.Root(10, 2, 0);
+        }
+
+        [Test, ExpectedException(typeof(OverflowException))]
+        public void Root_OverflowExceptionTest()
+        {
+            Newton.Root(100, 100000);
+        }
     }
 }
