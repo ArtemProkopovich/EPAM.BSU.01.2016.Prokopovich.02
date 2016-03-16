6829545d0ab7d418db691afee0f68c87d44001f8
ArtemProkopovich <artemprokopovich96@gmail.com>
2016-03-17 01:12:28 +0300

    Реализован класс IntHexFormatProvider реализующий интерфесы IFormatProvider, ICustomProvider. Изменены Unit-тесты для класса

---
+++ Task3/IntHexFormatProvider.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Task3
+{
+    public class IntHexFormatProvider: IFormatProvider, ICustomFormatter
+    {
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            if (arg is int && (string.IsNullOrEmpty(format) || format.ToUpper(CultureInfo.InvariantCulture) == "H"))
+            {
+                return "0x" + IntToHex((int) arg);
+            }
+            else
+            {
+                try
+                {
+                    return HandleOtherFormats(format, arg);
+                }
+                catch (FormatException e)
+                {
+                    throw new FormatException($"The format of '{format}' is invalid.", e);
+                }
+            }
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            return formatType == typeof(ICustomFormatter) ? this : null;
+        }
+
+        private string IntToHex(int number)
+        {
+            if (number == 0)
+                return "0";
+            bool neg = number < 0;
+            number = Math.Abs(number);
+            char A = 'A';
+            string result = "";
+            while (number != 0)
+            {
+                int mod = number % 16;
+                if (mod < 10)
+                    result += mod;
+                else
+                    result += (char)(A + mod - 10);
+                number /= 16;
+            }
+            if (neg)
+                result += '-';
+            result = new string(result.Reverse().ToArray());
+            return result;
+        }
+
+        private string HandleOtherFormats(string format, object arg)
+        {
+            if (arg is IFormattable)
+                return ((IFormattable)arg).ToString(format, CultureInfo.CurrentCulture);
+            else if (arg != null)
+                return arg.ToString();
+            else
+                return String.Empty;
+        }
+    }
+}
--- Task3_NUnitTest/Task3_Test.cs
+++ Task3_NUnitTest/Task3_Test.cs
@@ -5,21 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Task3;
+using System.Globalization;
+using System.Threading;
 
 namespace Task3_NUnitTest
 {
     [TestFixture]
     public class Task3_Test
     {
-        [Test, TestCase(64, Result = "40")]
-        [TestCase(100, Result = "64")]
-        [TestCase(-100, Result = "-64")]
-        [TestCase(0, Result = "0")]
-        [TestCase(25485, Result = "638D")]
-        [TestCase(88979905, Result = "54DB9C1")]
+        [Test, TestCase(64, Result = "0x40")]
+        [TestCase(100, Result = "0x64")]
+        [TestCase(-100, Result = "0x-64")]
+        [TestCase(0, Result = "0x0")]
+        [TestCase(25485, Result = "0x638D")]
+        [TestCase(88979905, Result = "0x54DB9C1")]
+        [TestCase(47.2, ExpectedException = typeof(ArgumentException))]
         public string IntExtension_Test(int number)
         {
-            return number.HexFormat();
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            IFormatProvider fp = new IntHexFormatProvider();
+            return string.Format(fp, "{0:H}", number);
+        }
+
+        [TestCase(47, "{0:X}", Result = "2F")]
+        [TestCase(.473, "{0:P}", Result = "47.30 %")]
+        [TestCase(.473, "{0:P0}", Result = "47 %")]
+        [TestCase(4.73, "{0:C}", Result = "¤4.73")]
+        [TestCase(4.73, "{0:C}", Result = "¤4.73")]
+        [TestCase(4.7321, "{0:F2}", Result = "4.73")]
+        public string ParentFormat_Test(object number, string format)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            IFormatProvider fp = new IntHexFormatProvider();
+            return string.Format(fp, format, number);
         }
     }
 }
