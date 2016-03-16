4652cabfb107fa954c916ececeaa5fbf59586a9d
ArtemProkopovich <artemprokopovich96@gmail.com>
2016-03-17 00:45:10 +0300

    Реализован CustomFormatProvider для класса Customer

---
+++ Task4/CustomerFormatProvider.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Task4
+{
+    public class CustomerFormatProvider : IFormatProvider, ICustomFormatter
+    {
+        public object GetFormat(Type formatType)
+        {
+            return formatType == typeof(ICustomFormatter) ? this : null;
+        }
+
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            Customer customer = null;
+            if (!(arg is Customer))
+                try
+                {
+                    return HandleOtherFormats(format, arg);
+                }
+                catch (FormatException e)
+                {
+                    throw new FormatException($"The format of '{format}' is invalid.", e);
+                }
+            customer = (Customer)arg;
+            if (String.IsNullOrEmpty(format)) return customer.ToString("N,P,R", formatProvider);
+            if (formatProvider == null) formatProvider = CultureInfo.CurrentCulture;
+            string result = "";
+            foreach (string param in format.ToUpperInvariant().Split(','))
+            {
+                bool isFound = false;
+                switch (param)
+                {
+                    case "N":
+                        result += "Name: " +  customer.Name.ToString(CultureInfo.CurrentCulture) + ", ";
+                        isFound = true;
+                        break;
+                    case "P":
+                        result += "Phone: " + customer.Phone.ToString(CultureInfo.CurrentCulture) + ", ";
+                        isFound = true;
+                        break;
+                    case "R":
+                        result += "Revenue: " + customer.Revenue.ToString(CultureInfo.CurrentCulture) + ", ";
+                        isFound = true;
+                        break;
+                }
+                Regex revenueRegex = new Regex(@"\A[R](:[A-Z]-?[0-9]?)$");
+                if (revenueRegex.IsMatch(param))
+                {
+                    result += "Revenue: " + customer.Revenue.ToString(param.Remove(0, 2), CultureInfo.CurrentCulture) + ", ";
+                    isFound = true;
+                }
+                if (!isFound)
+                    throw new FormatException($"The {param} format string is not supported.");
+            }
+            result = result.TrimEnd(',', ' ');
+            result += ".";
+            return result;
+        }
+
+        private string HandleOtherFormats(string format, object arg)
+        {
+            if (arg is IFormattable)
+                return ((IFormattable)arg).ToString(format, CultureInfo.CurrentCulture);
+            else if (arg != null)
+                return arg.ToString();
+            else
+                return String.Empty;
+        }
+    }
+}
--- Task4_NUnitTest/Customer_ToFormatStringTest.cs
+++ Task4_NUnitTest/Customer_ToFormatStringTest.cs
@@ -5,24 +5,27 @@ using Task4;
 namespace Task4_NUnitTest
 {
     [TestFixture]
-    public class Customer_ToFormatStringTest
+    public class CustomerFormatProvider_Test
     {
         [Test]
-        [TestCase("N,P,R:C2")]
-        [TestCase("N,R:F2,P")]
+        [TestCase("{0:N,P,R:C2}")]
+        [TestCase("{0:N,R:F2,P}")]
         public void Customer_ToStringExtensionTestMethod(string format)
         {
             Customer customer = new Customer() { Name = "Jeff", Phone = "+375(29)000-00-00", Revenue = 10000.53m };
-            Assert.AreNotEqual(customer.ToFormatString(format), string.Empty);
+            string result = String.Format(new CustomerFormatProvider(), format, customer);
+            Assert.AreNotEqual(result, string.Empty);
         }
 
         [Test, ExpectedException(typeof (FormatException))]
-        [TestCase("N,P,R:C23")]
-        [TestCase("N,T:D2,P")]
+        [TestCase("{0:N,P,R:C23}")]
+        [TestCase("{0:N,T:D2,P}")]
+        [TestCase("{0:123}")]
         public void Customer_ToStringExtension_Exception_TestMethod(string format)
         {
             Customer customer = new Customer() {Name = "Jeff", Phone = "+375(29)000-00-00", Revenue = 10000.53m};
-            customer.ToFormatString(format);
+            string result = String.Format(new CustomerFormatProvider(), format, customer);
+            Assert.AreNotEqual(result, string.Empty);
         }
     }
 }
